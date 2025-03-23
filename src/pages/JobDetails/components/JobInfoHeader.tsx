f9bc9b2a848493383a98bbc8e46a9b62715d9f68
gazanfarli <qznfrli.2001@gmail.com>
2025-03-23 13:50:10 +0400

    Tried to resolve checking file names case sensitive when building

--- src/pages/JobDetails/components/JobInfoHeader.tsx
+++ src/pages/JobDetails/components/JobInfoHeader.tsx
@@ -1,4 +1,4 @@
-import Badge from "../../../components/ui/badge";
+import Badge from "../../../components/ui/Badge";
 import favoriteIcon from "../../../assets/favorites.svg";
 
 type JobInfoHeaderProps = {
